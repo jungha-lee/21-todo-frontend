ed8648d5dca9a47af89dc5ddb2403cc332e53486
Jungha Lee <junghalee001@gmail.com>
2021-10-16 19:58:24 +0200

    Add AddTodo function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Todo from './Todo';
-import { Paper, List } from '@material-ui/core';
+import AddTodo from './AddTodo';
+import { Paper, List, Container } from '@material-ui/core';
 import './App.css';
 
 class App extends React.Component {
@@ -8,11 +9,20 @@ class App extends React.Component {
         super(props);
         this.state = {
             items: [
-                { id: 0, title: 'Hello World 1', done: true },
-                { id: 1, title: 'Hello World 2', done: false },
+                { id: "0", title: 'Hello World 1', done: true },
+                { id: "1", title: 'Hello World 2', done: false },
             ],
         };
     }
+    add = (item) => {
+        const thisItems = this.state.items;
+        item.id = 'ID-' + thisItems.length;
+        item.done = false;
+        thisItems.push(item);
+        this.setState({ items: thisItems });
+        console.log('items : ', this.state.items);
+    };
+
     render() {
         let todoItems = this.state.items.length > 0 && (
             <Paper style={{ margin: 16 }}>
@@ -23,7 +33,14 @@ class App extends React.Component {
                 </List>
             </Paper>
         );
-        return <div className="App">{todoItems}</div>;
+        return (
+            <div className="App">
+                <Container maxWidth="md">
+                    <AddTodo add={this.add}/>
+                    <div className="TodoList">{todoItems}</div>
+                </Container>
+            </div>
+        );
     }
 }
 
