3f0c3c99dd9be9f8d48a4d88eb656a814b44c40e
Jungha Lee <junghalee001@gmail.com>
2021-10-27 18:56:54 +0200

    Implement log out function and fix UI glitch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import Todo from './Todo';
 import AddTodo from './AddTodo';
-import { call } from './service/ApiService';
-import { Paper, List, Container } from '@material-ui/core';
+import { call, signout } from './service/ApiService';
+import {
+    Paper,
+    List,
+    Container,
+    Grid,
+    Button,
+    AppBar,
+    Toolbar,
+    Typography,
+} from '@material-ui/core';
 import './App.css';
 
 class App extends React.Component {
@@ -10,11 +19,12 @@ class App extends React.Component {
         super(props);
         this.state = {
             items: [],
+            loading: true,
         };
     }
     componentDidMount() {
         call('/todo', 'GET', null).then((response) =>
-            this.setState({ items: response.data })
+            this.setState({ items: response.data, loading: false })
         );
     }
 
@@ -51,14 +61,41 @@ class App extends React.Component {
                 </List>
             </Paper>
         );
-        return (
-            <div className="App">
+
+        var navigationBar = (
+            <AppBar position="static">
+                <Toolbar>
+                    <Grid justify="space-between" container>
+                        <Grid item>
+                            <Typography variant="h6">Today's todo</Typography>
+                        </Grid>
+                        <Grid>
+                            <Button color="inherit" onClick={signout}>
+                                Log out
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+        );
+
+        let todoListPage = (
+            <div>
+                {navigationBar}
                 <Container maxWidth="md">
                     <AddTodo add={this.add} />
                     <div className="TodoList">{todoItems}</div>
                 </Container>
             </div>
         );
+        let loadingPage = <h1> Loading... </h1>;
+
+        let content = loadingPage;
+        if (!this.state.loading) {
+            content = todoListPage;
+        }
+
+        return <div className="App">{content}</div>;
     }
 }
 
